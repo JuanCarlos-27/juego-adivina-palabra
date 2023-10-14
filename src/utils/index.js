0d85ea4b55cc87cb6df28825beaf154ca278ab25
JuanCarlos-27 <jcromero909@misena.edu.co>
2023-10-14 17:52:41 -0500

    Add win logic

---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+export const WORDS = [
+    'computer',
+    'bicycle',
+    'helicopter',
+    'calendar',
+    'automobile',
+    'elephant',
+    'piano',
+    'giraffe',
+    'shirt',
+    'beach'
+];
+export const MOVEMENTS = {
+    UP: 'ArrowUp',
+    DOWN: 'ArrowDown',
+    LEFT: 'ArrowLeft',
+    RIGHT: 'ArrowRight'
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'custom-purple-dark': '#7429C6',
+				'custom-pink': '#C951E7',
+				'custom-gray': '#4A5567',
+				'custom-white': '#F2F5F9',
+				'custom-dark': '#030616'
+			}
+		},
 	},
 	plugins: [],
 }
